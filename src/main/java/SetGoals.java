bddbf81c49e17947c23b121c2ca97655b0cb0613
Oluwalademi Aromolaran <oluwalademiaromolaran@gmail.com>
2025-11-16 19:54:04 -0600

    Create skeleton and plan for saving goals and tracking progress use case implementation - includes errors

---
+++ src/main/java/SetGoals.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SetGoals {
+    public void displayGoalSettingInterface() {
+
+    }
+
+    public void selectOverallGoal(goalType) {
+
+    }
+
+    public void defineTargets(Detail detail) {
+
+    }
+
+    public void adjustPreferences(frequency, intensity) {}
+
+    public void saveGoals() {}
+
+    public void cpnfirmGoalSaved() {}
+
+    public void logProgressEntry(data) {}
+
+    public void updateProgressCharts(metrics, graphs) {}
+
+    public void displayMilestones(achievementBadge) {}
+}
+
+// TODO: List of things to do
+// 1. Create a Goal Class
+// 2. Link Goals to Data/AddData Class
+// 3. Create ProgressTracker class
+// 4. Create Milestone object
+// 5. Align code with previously created analysis/design diagrams
+// 6. Work on Track Progress part of this use case
+// 7. Work on use case: Create Self-Paced Exercise Plans
