969e178444e42f12cb0cfc68557ac1fb4dbac4e2
jonahb023 <134671079+jonahb023@users.noreply.github.com>
2025-12-04 12:06:36 -0600

    Added setters needed for modify classes

--- src/main/java/myPackage/WorkoutClass.java
+++ src/main/java/myPackage/WorkoutClass.java
@@ -20,6 +20,7 @@ public class WorkoutClass {
         this.cost = cost;
     }
 
+    //Getters
     public int getId() {
         return id;
     }
@@ -50,6 +51,39 @@ public class WorkoutClass {
 
     public double getCost() {
         return cost;
+    }
+    
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTrainerUsername(String trainerUsername) {
+        this.trainerUsername = trainerUsername;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
     }
 
     @Override
